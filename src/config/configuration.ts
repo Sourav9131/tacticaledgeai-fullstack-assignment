17a2859f4e457452860e93a99ecda62a48937eac
Sourav9131 <124760869+Sourav9131@users.noreply.github.com>
2026-02-01 23:39:36 +0530

    Set default MongoDB URI in configuration

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,7 +1,7 @@
 export default () => ({
   port: parseInt(process.env.PORT, 10) || 3000,
   database: {
-    uri: process.env.MONGODB_URI ,
+      uri: process.env.MONGODB_URI  || "mongodb+srv://sourav:w!JkAerL3ej4-6Z@chuffers.5fiipup.mongodb.net/movie-database?retryWrites=true&w=majority" ,
   },
   jwt: {
     secret: process.env.JWT_SECRET || 'dev_media_backend_secret_2026',
